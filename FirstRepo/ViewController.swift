e885438959177e545b53522dcc8aa9842b2d7a40
PASSARIT ATIWORRAMAN <ntseries@gmail.com>
2017-02-17 02:22:17 +0700

    Initial Commit

---
+++ FirstRepo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FirstRepo
+//
+//  Created by PASSARIT ATIWORRAMAN on 2/17/2560 BE.
+//  Copyright © 2560 PASSARIT ATIWORRAMAN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
